617ad327c207f4856e7741cd315fbe5e8b4a3486
james-watkin <jameswatkins1260@gmail.com>
2020-12-18 17:17:04 -0800

    do it with adjacent elements

--- AlgoExpert/AlgoExp_easy.js
+++ AlgoExpert/AlgoExp_easy.js
@@ -10,4 +10,31 @@ constructor(value) {
     this.right = null;
 }
 }
+
+// Do it with adjacent elements.
   
+function minMoves(arr) {
+    // Write your code here
+    let leftIdx = 0; // 0
+    let rightIdx = arr.length - 1; // 8
+    let total = 0;
+    
+    while(leftIdx !== rightIdx){ // 0 !== 8
+        //  [ 1,0,1,0,0,0,0,1 ]
+        // 1 === 1, 1 === 0
+        if(arr[leftIdx] === 1 && arr[rightIdx] === 0){
+            [arr[leftIdx],arr[rightIdx]] = [arr[rightIdx], arr[leftIdx]]
+            console.log(arr)
+            total += 1
+            leftIdx += 1
+            rightIdx -= 1
+        } else if(arr[rightIdx] === 1){
+            rightIdx -= 1
+        } else {
+            leftIdx += 1
+        }
+        
+    }
+    return total
+    
+}
