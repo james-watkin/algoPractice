7515d60014e94190d99f47f4389214e33e33f14b
james-watkin <jameswatkins1260@gmail.com>
2020-12-21 21:49:06 -0800

    grid traveler

--- AlgoExpert/DynProg.js
+++ AlgoExpert/DynProg.js
@@ -1,10 +1,25 @@
-const fib = (n) => {
-  if(n <= 2) return 1;
-  return fib(n-1) + fib(n-2)  
+// memoization
+// js obj, keys will be arg to fn, value will be the return value
+
+const fib = (n, memo = {}) => {
+    if(n in memo) return memo[n];
+    if(n <= 2) return 1;
+
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    console.log(memo)
+    return memo[n];  
 };
 
-console.log(fib(6));
-console.log(fib(7));
-console.log(fib(8));
+// O(n) Time
+// O(n) Space
 
-console.log(fib(40))
+// console.log(fib(6));
+// console.log(fib(7));
+// console.log(fib(8));
+
+// console.log(fib(10))
+
+
+const gridTraveler = (l,w) => {
+    
+}
