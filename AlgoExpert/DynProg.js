fcaa68809e94abd8a23ec3b6139656b47fc883d2
james-watkin <jameswatkins1260@gmail.com>
2020-12-24 14:24:18 -0800

    how sum dynamic prog

--- AlgoExpert/DynProg.js
+++ AlgoExpert/DynProg.js
@@ -36,6 +36,34 @@ const gridTraveler = (l,w, memo = {}) => {
 
 
 // 7, [5,3,4,7] => true
-const canSun = (targetSum, number) => {
+const canSum = (targetSum, numbers, memo = {} ) => {
+    if( targetSum in memo) return memo[targetSum];
+    if( targetSum === 0 ) return true;
+    if( targetSum < 0) return false;
+
+    for( let num of numbers )  {
+        const remainder = targetSum - num;
+        if(canSum(remainder, numbers, memo)){
+            memo[targetSum] = true
+            return true;
+        }
+    }
+    memo[targetSum] = false;
+    return false;
+}
+
+// console.log(canSum(7,[2, 3]))
+// console.log(canSum(7,[5,3,4,7]))
+// console.log(canSum(7,[2, 4]))
+// console.log(canSum(8,[2, 3, 5 ]))
+// console.log(canSum(13000,[7,14]))
+
+const howSum = (targetSum, numbers) => {
 
 }
+
+// console.log(howSum(7,[2, 3]))
+// console.log(howSum(7,[5,3,4,7]))
+// console.log(howSum(7,[2, 4]))
+// console.log(howSum(8,[2, 3, 5 ]))
+// console.log(howSum(300,[7,14]))
