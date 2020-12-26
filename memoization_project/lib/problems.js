0e20f10356f399649a279bb81150ce56880f3372
james-watkin <jameswatkins1260@gmail.com>
2020-12-25 16:09:39 -0800

    tabulation added

--- memoization_project/lib/problems.js
+++ memoization_project/lib/problems.js
@@ -39,7 +39,20 @@ function lucasNumberMemo(n, memo = {}) {
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount, memo = {}) {
+    if( amount in memo ) return memo[amount]
+    if( amount === 0) return 0;
 
+    let numCoins = []
+
+    coins.forEach(coin =>{
+        if(coin <= amount){
+            let nextAmount = amount - coin
+            numCoins.push(minChange(coins, nextAmount, memo) + 1)
+        }
+    })
+
+    memo[amount] = Math.min(...numCoins)
+    return memo[amount]
 }
 
 
