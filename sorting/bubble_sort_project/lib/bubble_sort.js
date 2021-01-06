1d1e5866bae4e8bc908b5ad813f7866189a3d87d
james-watkin <jameswatkins1260@gmail.com>
2021-01-06 01:15:15 -0800

    bubble sort review

--- sorting/bubble_sort_project/lib/bubble_sort.js
+++ sorting/bubble_sort_project/lib/bubble_sort.js
@@ -1,9 +1,22 @@
 function swap(array, idx1, idx2) {
-
+    [array[idx1], array[idx2]] = [array[idx2],array[idx1]]
+    return array;
 }
 
 function bubbleSort(array) {
 
+    sorted = false;
+    while(!sorted){
+        sorted = true;
+        for(let i = 0; i < array.length - 1 ; i++){
+            if(array[i] > array[i+1]){
+                swap(array, i, i+1)
+                sorted = false;
+            }
+        }
+    }
+
+    return array;
 }
 
 
