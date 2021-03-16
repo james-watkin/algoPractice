1a701eda166fba46b6243f4d76872cc4dcc8a03a
james-watkin <jameswatkins1260@gmail.com>
2021-03-16 15:03:07 -0700

    :sparkles: fixed

--- AlgoExpert/firstDuplicate.js
+++ AlgoExpert/firstDuplicate.js
@@ -1,6 +1,6 @@
 // Given integers in an array 1 to n inclusive find the first duplicate value or return -1
 
-//Using a hash Time: O(n) Space: O(n)
+// Using a hash Time: O(n) Space: O(n)
 function firstDuplicateValue(array) {
     let hash = {}
       
@@ -13,4 +13,23 @@ function firstDuplicateValue(array) {
           
       }
     return -1;
-  }
+}
+
+// Why 1 to n inclusive?
+// Due to it always being a value > 1 we can use negatives to our advantage
+// Time: O(n)
+// Space: O(1)
+const firstDuplicates = (array) => {
+    
+	for(let i = 0; i < array.length; i++){
+		let currVal = Math.abs(array[i])
+		
+		if( array[currVal -1 ] < 0){
+			return currVal
+		}else{
+			array[currVal - 1] *= -1
+		}
+	}
+	
+    return -1;
+}
