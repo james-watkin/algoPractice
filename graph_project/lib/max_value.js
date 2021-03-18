5f8baf58dd7bf84cd835ecb24c0131174817717c
james-watkin <jameswatkins1260@gmail.com>
2021-03-18 00:09:14 -0700

    graph max value

--- graph_project/lib/max_value.js
+++ graph_project/lib/max_value.js
@@ -1,5 +1,19 @@
 function maxValue(node, visited=new Set()) {
+    let largest = -Infinity
+    let queue = [node]
 
+    while( queue.length) {
+        let node = queue.shift()
+
+        if(visited.has(node)) continue;
+        visited.add(node)
+
+        if( node.val > largest) largest = node.val;
+
+        queue = queue.concat(node.neighbors)
+    }
+
+    return largest
 }
 
 module.exports = {
