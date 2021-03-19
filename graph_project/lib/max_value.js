26f9d67cb766b573566ad1ff8e9862abdb116d37
james-watkin <jameswatkins1260@gmail.com>
2021-03-19 00:48:55 -0700

    max value refresher

--- graph_project/lib/max_value.js
+++ graph_project/lib/max_value.js
@@ -1,5 +1,19 @@
-function maxValue(node) {
+function maxValue(node, visited = new Set()) {
 
+    let queue = [node]
+
+    let largest = -Infinity
+    while(queue.length){
+        let node = queue.shift()
+        if(visited.has(node)) continue;
+        visited.add(node)
+
+        if(node.val > largest) largest = node.val;
+        queue.push(...node.neighbors)
+
+    }
+
+    return largest
 }
 
 module.exports = {
