cd493743fc861bfb84c6890bc7b399c629d0cdaf
james-watkin <jameswatkins1260@gmail.com>
2021-04-13 23:55:37 -0700

    longest path through matrix

--- interviewPractice.js
+++ interviewPractice.js
@@ -18,29 +18,102 @@
 // totalAmount before a negative number
 // array of totals -> get added once a reset happens
 
-function Kadanes(array) {
-    let runningSum = array[0];  
-    let maxNum = -Infinity;   
-    for(let i = 1 ; i < array.length ; i++ ){
-        let curEle = array[i]               
-        let potSum = runningSum + curEle   
+// function Kadanes(array) {
+//     let runningSum = array[0];  
+//     let maxNum = -Infinity;   
+//     for(let i = 1 ; i < array.length ; i++ ){
+//         let curEle = array[i]               
+//         let potSum = runningSum + curEle   
 
-        if( potSum <= 0 ){ 
-            maxNum = Math.max(runningSum, maxNum)
-            runningSum = 0;
+//         if( potSum <= 0 ){ 
+//             maxNum = Math.max(runningSum, maxNum)
+//             runningSum = 0;
 
-        }else {
-            runningSum += curEle
-            maxNum = Math.max(runningSum, maxNum)
+//         }else {
+//             runningSum += curEle
+//             maxNum = Math.max(runningSum, maxNum)
+//         }
+
+
+//     }
+
+//     return maxNum
+
+// }
+
+// console.log(Kadanes([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 1, 3,-5, 4]))
+// console.log(Kadanes([-2,-3,-4,-5,-6,-7,-8,-9,-10]))
+
+
+function longestPath(matrix){
+    let startArray = new Array(matrix.length).fill().map( u => new Array(matrix[0].length).fill(null) )
+    for(let i = 0; i < matrix.length; i++){
+        let currentRow = startArray[i]
+        for( let j = 0; j < currentRow.length; j++){
+            let currentInMatrix = matrix[i][j]
+
+            if( currentInMatrix === 0 ) startArray[i][j] = 1;
         }
-
-
     }
 
-    return maxNum
+    for( let i = 0; i < startArray.length; i++){
+        let currentRow = startArray[i]
+        for(let j = 0; j < startArray[0].length; j++){
+            let currentEle = currentRow[j]
+            if(currentEle === null) continue;
+            //Search Left
+            let left = j - 1
+            while(left >= 0){
+                let currentEle = currentRow[left]
+                if(currentEle === null){
+                    break
+                }else{
 
+                    currentRow[left] += 1
+                    left--
+                }
+            }
+            //Search Right
+            let right = j + 1
+            while(right < currentRow.length){
+                let currentEle = currentRow[right]
+                if(currentEle === null){
+                    break;
+                }else{
+
+                    currentRow[right] += 1
+                    right++
+                }
+            }
+            //Search Up
+            
+            if( i > 0){
+                
+                let above = startArray[i-1][j]
+                if( above !== null){ 
+                    currentRow[j] += above 
+                }
+                else{
+                    let maxInRange = Math.max(...currentRow.slice(left + 1 , right - 1))
+                    currentRow[j] = Math.max(currentRow[j], maxInRange)
+                }
+            }
+
+        }
+        
+    }
+    let solution = Math.max(...startArray[startArray.length - 1])
+    console.log(startArray)
+    if(solution === 1) return [-1,-1]
+    return solution;
 }
 
-console.log(Kadanes([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 1, 3,-5, 4]))
-console.log(Kadanes([-2,-3,-4,-5,-6,-7,-8,-9,-10]))
+// [0,0,1,1,0]
+// [0,0,1,1,0]
+// [1,0,0,1,1]
+// [1,1,0,1,1]
+// [0,0,0,1,1]
+
+console.log(longestPath([ [0,0,'#','#',0], [0,0,'#','#',0], ['#',0,0,'#',0], ['#','#',0,'#',0], [0,0,0,"#","#"]])) // -> 4
+console.log(longestPath([ [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]))
 
