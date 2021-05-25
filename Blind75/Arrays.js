479b4458acd777efe323e53fac050a326f379350
james-watkin <jameswatkins1260@gmail.com>
2021-05-25 16:05:13 -0700

    contains duplicates

--- Blind75/Arrays.js
+++ Blind75/Arrays.js
@@ -40,8 +40,6 @@ function maxProfit(array){
         let current = array[i]
         minPrice = Math.min(current,minPrice)
         if(current > minPrice){
-            // console.log(current)
-            // console.log(minPrice)
             profit = Math.max(profit, current - minPrice)
         }
     }
@@ -51,5 +49,29 @@ function maxProfit(array){
     return profit
 }
 
-console.log(maxProfit([7,1,5,3,6,4])) // -> 5
-console.log(maxProfit([7,6,4,3,1])) // -> 0
+// console.log(maxProfit([7,1,5,3,6,4])) // -> 5
+// console.log(maxProfit([7,6,4,3,1])) // -> 0
+
+
+// ! Duplicate Numbers
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+function containsDuplicate(array){
+
+    let hash = {}
+
+    for( let i = 0; i < array.length; i++){
+        let ele = array[i]
+        if( hash[ele] ) return true;
+        hash[ele] = true;
+    }
+
+    return false;
+}
+
+
+// console.log(containsDuplicate([1,2,3,1])) // True
+// console.log(containsDuplicate([1,2,3,4])) // False
+// console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2])) // True
