a704b3433407c632cd4dd7d7cd58e1733c0566cd
james-watkin <jameswatkins1260@gmail.com>
2021-07-12 23:54:49 -0700

    couple changes

--- Blind75/Arrays.js
+++ Blind75/Arrays.js
@@ -130,9 +130,9 @@ function maxSubArray(array){
     return currentMax
 }
 
-console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])) // 6
-console.log(maxSubArray([1])) // 1
-console.log(maxSubArray([5,4,-1,7,8])) // 23
+// console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])) // 6
+// console.log(maxSubArray([1])) // 1
+// console.log(maxSubArray([5,4,-1,7,8])) // 23
 
 
 
@@ -145,6 +145,9 @@ console.log(maxSubArray([5,4,-1,7,8])) // 23
 // A subarray is a contiguous subsequence of the array.
 
 function maxProduct(array){
+   
+    
+    
 
 }
 
